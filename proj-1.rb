9427f55f57bc7ed5d162441810882b426ccb75bb
Praveen Kmaur <69117304+Praveencoder03@users.noreply.github.com>
2020-12-28 21:19:46 +0530

    Add files via upload

---
+++ proj-1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+#hello im prvaeen
+puts "I'd much rather you 'not'."
+#this is my first ruby project
+puts 'I "said" do not touch this.'
