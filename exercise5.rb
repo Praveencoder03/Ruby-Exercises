d60e4bedf7d6ddb205068d26a26b135f14451aad
Praveen Kmaur <69117304+Praveencoder03@users.noreply.github.com>
2021-01-05 19:07:20 +0530

    Add files via upload

---
+++ exercise5.rb
@@ -0,0 +1,14 @@
+my_name = 'Praveen'
+my_age = 24
+my_height =180
+my_weight=89
+my_eyes='black&brown'
+my_teeth='white'
+my_hair='Brown'
+
+puts "Lets talk about #{my_name}."
+puts "he's  #{my_height}  inches tall."
+puts "he's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair}hair."
+puts "he's teeth are usually #{my_teeth} depending on the coffee."
